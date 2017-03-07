4dbe23b8a74f6a0fe5b5bf9400883aef6b69f542
MAIN\thb7554 <thb7554@rit.edu>
2017-03-07 12:19:22 -0500

    halfway there

--- E09_Gimbal_Lock/AppClass.cpp
+++ E09_Gimbal_Lock/AppClass.cpp
@@ -40,6 +40,9 @@ void AppClass::Update(void)
 	m_pMeshMngr->AddInstanceToRenderList("Steve");
 
 	int nFPS = m_pSystem->GetFPS();
+
+	static DWORD startTimeSystem = GetTickCount();
+
 	m_pMeshMngr->PrintLine("");
 	m_pMeshMngr->PrintLine(m_pSystem->GetAppName(), REYELLOW);
 	m_pMeshMngr->Print("X:", REYELLOW);
@@ -51,6 +54,19 @@ void AppClass::Update(void)
 
 	m_pMeshMngr->Print("FPS:");
 	m_pMeshMngr->Print(std::to_string(nFPS), RERED);
+	
+	matrix4 m4sphere1;
+	float timer = (float)((GetTickCount() - startTimeSystem) / 1000.0f);
+	float timerMapped = MapValue(timer, 0.0f, 5.0f, 0.0f, 1.0f);
+	
+	m_pMeshMngr->AddSphereToRenderList(m4sphere1, RERED, WIRE);
+	m_pMeshMngr->PrintLine("");
+	m_pMeshMngr->PrintLine("Actual time is :" + std::to_string(timer), REYELLOW);
+
+	vector3 v3lerp = glm::lerp(vector3(0, 0, 0), vector3(1, 0, 0), ((float)(GetTickCount() - startTimeSystem) / 1000.0f));
+	m4sphere1 = glm::translate(v3lerp);
+	m_pMeshMngr->SetModelMatrix(m4sphere1);
+
 }
 
 void AppClass::Display(void)
